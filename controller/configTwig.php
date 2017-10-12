f96ad9906e207c93a0c2cd26a99464c713a49f4e
Marcelo Rodriguez <marcelorodriguez37@gmail.com>
2017-10-11 22:54:42 -0300

    twig y otras configuraciones

---
+++ controller/configTwig.php
@@ -0,0 +1,20 @@
+<?php 
+  function renderizar2($view,$variable){
+		require_once '../Twig-1.16.2/lib/Twig/Autoloader.php';
+		Twig_Autoloader::register();
+		$loader = new Twig_Loader_Filesystem('../view/');
+		$twig = new Twig_Environment($loader, array());
+		$template = $twig->loadTemplate( $view );
+		$template->display($variable);
+	}
+
+	function renderizar($view){
+		require_once '../Twig-1.16.2/lib/Twig/Autoloader.php';
+		Twig_Autoloader::register();
+		$loader = new Twig_Loader_Filesystem('../view/');
+		$twig = new Twig_Environment($loader, array());
+		$template = $twig->loadTemplate( $view );
+		$template->display(array());
+	}
+
+ ?>
