cc9ae75c06de452ba3fcc4ab63184f1062e54a08
Brenda Dominguez <dominguezbrenda@hotmail.com.ar>
2017-10-23 21:09:34 -0300

    cambios en la BD chelo

--- gimnasio.sql
+++ gimnasio.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2017 a las 04:56:54
--- Versión del servidor: 10.1.8-MariaDB
--- Versión de PHP: 5.6.14
+-- Tiempo de generación: 24-10-2017 a las 02:07:39
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,7 +32,7 @@ CREATE TABLE `abono` (
   `idCliente` int(20) NOT NULL,
   `fecha` date NOT NULL,
   `monto` int(30) NOT NULL,
-  `habilitado` int(2) NOT NULL
+  `habilitado` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,7 +44,7 @@ CREATE TABLE `abono` (
 CREATE TABLE `actividad` (
   `id` int(11) NOT NULL,
   `nombre` varchar(40) NOT NULL,
-  `habilitado` int(2) NOT NULL
+  `habilitado` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,7 +57,7 @@ CREATE TABLE `asistencia` (
   `id` int(11) NOT NULL,
   `idCliente` int(30) NOT NULL,
   `fecha` datetime NOT NULL,
-  `habilitado` int(2) NOT NULL
+  `habilitado` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,7 +76,7 @@ CREATE TABLE `cliente` (
   `telefono` int(11) NOT NULL,
   `fechaInscripcion` date NOT NULL,
   `observaciones` text,
-  `habilitado` int(2) NOT NULL
+  `habilitado` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,7 +90,18 @@ CREATE TABLE `pack` (
   `nombre` varchar(40) NOT NULL,
   `precio` int(11) NOT NULL,
   `descripcion` text,
-  `habilitado` int(2) NOT NULL
+  `habilitado` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pack_actividad`
+--
+
+CREATE TABLE `pack_actividad` (
+  `id_pack` int(11) NOT NULL,
+  `id_actividad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -101,7 +112,8 @@ CREATE TABLE `pack` (
 -- Indices de la tabla `abono`
 --
 ALTER TABLE `abono`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPack` (`idPack`);
 
 --
 -- Indices de la tabla `actividad`
@@ -113,7 +125,8 @@ ALTER TABLE `actividad`
 -- Indices de la tabla `asistencia`
 --
 ALTER TABLE `asistencia`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCliente` (`idCliente`);
 
 --
 -- Indices de la tabla `cliente`
@@ -126,6 +139,13 @@ ALTER TABLE `cliente`
 --
 ALTER TABLE `pack`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pack_actividad`
+--
+ALTER TABLE `pack_actividad`
+  ADD KEY `id_pack` (`id_pack`),
+  ADD KEY `id_actividad` (`id_actividad`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -156,6 +176,41 @@ ALTER TABLE `cliente`
 --
 ALTER TABLE `pack`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `abono`
+--
+ALTER TABLE `abono`
+  ADD CONSTRAINT `abono_ibfk_1` FOREIGN KEY (`idPack`) REFERENCES `pack` (`id`);
+
+--
+-- Filtros para la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  ADD CONSTRAINT `actividad_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pack_actividad` (`id_actividad`);
+
+--
+-- Filtros para la tabla `asistencia`
+--
+ALTER TABLE `asistencia`
+  ADD CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`);
+
+--
+-- Filtros para la tabla `pack`
+--
+ALTER TABLE `pack`
+  ADD CONSTRAINT `pack_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pack_actividad` (`id_pack`);
+
+--
+-- Filtros para la tabla `pack_actividad`
+--
+ALTER TABLE `pack_actividad`
+  ADD CONSTRAINT `pack_actividad_ibfk_1` FOREIGN KEY (`id_pack`) REFERENCES `pack` (`id`),
+  ADD CONSTRAINT `pack_actividad_ibfk_2` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
