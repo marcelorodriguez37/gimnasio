748c72c894587cb737572d1088a49cccb4b5b66a
brenda dominguez <dominguezbrenda@hotmail.com.ar>
2018-05-14 22:12:29 -0300

    Se agrega listado de deudores

--- model/ABMIngresos.php
+++ model/ABMIngresos.php
@@ -19,6 +19,16 @@
 		$conexion->desconectarBD();
 		return $res;
 	}
+	function listaDeudoresPorRango($fechaDesde, $fechaHasta){
+		require_once('conexion.php');
+		$conexion = new Conexion();
+		$conexion->conectarBD();
+		$query = $conexion -> getConexion() -> prepare("SELECT c.nombre as nombre ,c.apellido as apellido, c.dni as dni from cliente c WHERE c.id NOT IN (SELECT idCliente FROM abono WHERE fecha BETWEEN ? AND ? )");
+		$query->execute(array($fechaDesde, $fechaHasta));
+		$res = $query -> fetchAll(PDO::FETCH_ASSOC);
+		$conexion->desconectarBD();
+		return $res;
+	}
 
 	function actualizar($dni){
 		require_once('conexion.php');
